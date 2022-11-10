02bf67f2dc3e6c4ca79a63590e50a44be3392e84
Scott <scotty6663@hotmail.com>
2022-11-10 00:16:29 +0000

    adding abstract weapon

---
+++ src/objects/weapons/AbstractWeapon.js
@@ -0,0 +1,13 @@
+
+export default class AbstractWeapon {
+    constructor(player) {
+        this.player = player;
+        if (this.constructor == AbstractWeapon) {
+            throw new Error("Abstract classes can't be instantiated.");
+        }
+    }
+
+    fire() {
+        throw new Error("Method 'fire()' must be implemented on weapon class " + this.constructor);
+    }
+}
---
+++ src/objects/weapons/MachineGun.js
@@ -0,0 +1,12 @@
+import AbstractWeapon from "./AbstractWeapon.js";
+import Ball from "../Ball.js";
+
+export default class MachineGun extends AbstractWeapon {
+    constructor(player) {
+        super(player)
+    }
+
+    fire() {
+        new Ball(this.player.scene, this.player.x, this.player.y);
+    }
+}

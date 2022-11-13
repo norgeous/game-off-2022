3432a1105b51b1c4583a609ec1927205eace1c06
norgeous <daniel@harte.me>
2022-11-13 14:58:12 +0000

    Update HelloWorldScene.js

--- src/scenes/HelloWorldScene.js
+++ src/scenes/HelloWorldScene.js
@@ -29,7 +29,7 @@ export default class HelloWorldScene extends Phaser.Scene {
       this.map.spawners.zombie.forEach(zombie => {
         this.zombieGroup.add(new Zombie(this, zombie.x+16, zombie.y-16));
       });
-    }, 2000);
+    }, 200);
     
     this.createPlayer();
   }

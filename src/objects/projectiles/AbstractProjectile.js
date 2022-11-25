8d365d9afd22a4f00334b2d9a76c017528e9ca39
norgeous <daniel@harte.me>
2022-11-25 01:46:59 +0000

    Update AbstractProjectile.js

---
+++ src/objects/projectiles/AbstractProjectile.js
@@ -0,0 +1,13 @@
+// bullets have
+
+// spriteSheetKey
+// matterBodyConfig (physics options)
+// muzzleExitSpeed
+// vector (direction only)
+// bulletSpread
+// damage
+// minDestroySpeed
+// lockRotationToMovementVector
+// lifespan
+// isExplosive
+// explodeOnContactMask

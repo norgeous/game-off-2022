b13d3dcc3fb44dc66854a658de04501507454c82
norgeous <daniel@harte.me>
2022-11-29 18:14:48 +0000

    Update GameOver.js

--- src/objects/overlays/GameOver.js
+++ src/objects/overlays/GameOver.js
@@ -17,7 +17,6 @@ export default class GameOver {
 
     scene.input.once('pointerdown', () => {
       gameOver.text.destroy();
-      // gameOver.text.setText('Scotty!');
       // TODO: go back to main menu here
     });
   }

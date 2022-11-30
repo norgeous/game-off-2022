2a655240f41147752724ee9f8b38694e88a944ee
Scott <scotty6663@hotmail.com>
2022-11-30 17:10:31 +0000

    zombie spawn bug

--- src/scenes/AbstractScene.js
+++ src/scenes/AbstractScene.js
@@ -78,13 +78,12 @@ export default class AbstractScene extends Phaser.Scene {
       this.spawner = setInterval(() => {
         if (!this.player.active) return;
         this.map.spawners.zombie.forEach(zombie => {
-          if (zombie.properties?.count !== undefined) {
-            if (zombie.properties?.count == 0) return;
-              else zombie.properties.count--;
-          }
-
           const isInPlayerRange = Phaser.Math.Distance.BetweenPoints(zombie, this.player) <= Config.SPAWN_RANGE;
           if (isInPlayerRange) {
+            if (zombie.properties?.count !== undefined) {
+              if (zombie.properties?.count == 0) return;
+              else zombie.properties.count--;
+            }
             this.zombieGroup.get(zombie.x + 16, zombie.y - 16); // get = create
           }
         });
